1a0cb687210588735febaefb7f408fea51f3e8f8
ivyugin <ilya.vyugin@gmail.com>
2021-01-15 02:08:09 +0300

    flexible

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,19 +1,21 @@
 export default function SearchForm() {
   return (
     <section className='Search'>
-      <h1 className='Search_title'>
-        Что творится в мире?
-      </h1>
-      <p className='Search_subtitle'>
-        Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.
-      </p>
-      <form className='Search_form'>
-        <input
-          className='Search_form-input'
-          placeholder='Введите тему новости'
-        />
-        <button className='Search_form-button'>Искать</button>
-      </form>
+      <div className='Search__body'>
+        <h1 className='Search__title'>
+          Что творится в мире?
+        </h1>
+        <p className='Search__subtitle'>
+          Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.
+        </p>
+        <form className='Search__form'>
+          <input
+            className='Search__form-input'
+            placeholder='Введите тему новости'
+          />
+          <button className='Search__form-button'>Искать</button>
+        </form>
+      </div>
     </section>
   );
 }
